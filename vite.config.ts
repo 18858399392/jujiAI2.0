f76cc50691b0847361937054c30faf1af4a136cc
18858399392 <1784870881@qq.com>
2026-01-18 19:22:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-      'process.env.API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY),
-'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY || env.GEMINI_API_KEY)
+'process.env.API_KEY': JSON.stringify("AIzaSyCqVEIrKOFFsMiYQkcyxGeach-P2Qvn07w"),
+'process.env.GEMINI_API_KEY': JSON.stringify("AIzaSyCqVEIrKOFFsMiYQkcyxGeach-P2Qvn07w")
       },
       resolve: {
         alias: {
